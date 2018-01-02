d22d6e5ac6a59261c9c30afadafe7251b3fc12b6
Erik Seifert <erik.seifert@b-connect.de>
2018-01-02 13:56:54 +0100

    Add FieldFormatter plugin

---
+++ README.md
@@ -0,0 +1,8 @@
+# Twig Extender
+
+Drupal module for pluggable twig functions. 
+
+## Versions
+
+Drupal 8.2 or lower: Use Twig Extender 8.1
+Drupal 8.3 or higher: Use Twig Extender 8.2
---
+++ modules/twig_extender_extras/src/Plugin/TwigPlugin/FieldFormatter.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Drupal\twig_extender_extras\Plugin\TwigPlugin;
+
+use Drupal\twig_extender\Plugin\Twig\TwigPluginBase;
+use Drupal\Component\Utility\NestedArray;
+
+/**
+ * The plugin to render a field with a specific formatter.
+ *
+ * @TwigPlugin(
+ *   id = "twig_extender_field_formatter",
+ *   label = @Translation("Get a formatted field"),
+ *   type = "filter",
+ *   name = "view",
+ *   function = "getField"
+ * )
+ */
+class FieldFormatter extends TwigPluginBase {
+
+  /**
+   * Implementation for render field formatter.
+   *
+   * @param FieldItemList $field
+   *  FieldItemList to display with formatter
+   *
+   * @param string $formatter
+   *  Formatter plugin id to use
+   *
+   * @param string $label
+   *  Label option for formatter, could be hidden|above|inside
+   *
+   * @param int maxItems
+   *  How many items to display.
+   *
+   * @param int $offset
+   *  Offset to start from.
+   *
+   * @param array $settings
+   *  Settings for formatter plugin.
+   *
+   */
+  public function getField($field, $formatter, $label = 'hidden', $settings = [], $maxItems = -1, $offset = 0) {
+    $formatterService = \Drupal::service('plugin.manager.field.formatter');
+    try {
+      $plugin = $formatterService->getDefinition($formatter);
+      $field_type = $field->getFieldDefinition()->getType();
+      $field_options = $formatterService->getOptions();
+      $default_settings = $formatterService->getDefaultsettings($formatter);
+      $settings = NestedArray::mergeDeep($default_settings, $settings);
+      $renderer = \Drupal::service('renderer');
+
+      if ($offset > 0) {
+        if (!$field->offsetExists($offset)) {
+          $build = [];
+          $renderer->addCacheableDependency($build, $field->getEntity());
+          return $build;
+        }
+        for ( $i = 0; $i < $offset; $i++) {
+          $field->removeItem(0);
+        }
+      }
+
+
+      if ($maxItems > 0 && $maxItems != $field->count())  {
+        $remove = ($field->count() - $maxItems);
+        for ( $i = 0; $i < $remove; $i++) {
+          $field->removeItem(1);
+        }
+      }
+
+      $field_to_view = $field->view([
+          'type' => $formatter,
+          'settings' => $settings,
+          'label' => $label
+        ]
+      );
+      return $field_to_view;
+    }
+    catch (\Exception $e){
+      \Drupal::logger('twig_extender_extras')->error($e->getMessage());
+      throw $e;
+    }
+  }
+
+}
