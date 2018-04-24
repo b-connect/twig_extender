6c8cf425b9fdcdddd0ce0cce69f2e6338df4ccf3
Merlin Tribukait <merlin.tribukait@b-connect.de>
2018-04-24 18:19:26 +0200

    Fix: Fixed the Block Configurations

--- src/Plugin/TwigPlugin/BlockCreate.php
+++ src/Plugin/TwigPlugin/BlockCreate.php
@@ -3,6 +3,9 @@
 namespace Drupal\twig_extender\Plugin\TwigPlugin;
 
 use Drupal\twig_extender\Plugin\Twig\TwigPluginBase;
+use Drupal\Core\Block\BlockPluginInterface;
+use Drupal\Core\Render\Element;
+use Drupal\Core\Cache\CacheableMetadata;
 
 /**
  * The plugin for check authenticated user.
@@ -20,15 +23,56 @@ class BlockCreate extends TwigPluginBase {
   /**
    * Implementation for render block.
    */
-  public function getBlock($pluginId, $conf = []) {
-    $block_plugin_manager = \Drupal::service('plugin.manager.block');
-    /** @var \Drupal\language\Plugin\Block\LanguageBlock $language_block */
-    $block = $block_plugin_manager->createInstance($pluginId, $conf);
-    if (!$block) {
+  public function getBlock($pluginId, $conf = [], $wrapper = TRUE)
+  {
+
+    $conf += ['label_display' => BlockPluginInterface::BLOCK_LABEL_VISIBLE];
+
+    /** @var \Drupal\Core\Block\BlockPluginInterface $block_plugin */
+    $block_plugin = \Drupal::service('plugin.manager.block')
+      ->createInstance($pluginId, $conf);
+
+    // Inject runtime contexts.
+    if ($block_plugin instanceof ContextAwarePluginInterface) {
+      $contexts = \Drupal::service('context.repository')->getRuntimeContexts($block_plugin->getContextMapping());
+      \Drupal::service('context.handler')->applyContextMapping($block_plugin, $contexts);
+    }
+
+    if (!$block_plugin->access(\Drupal::currentUser())) {
       return;
     }
-    $build = $block->build();
-    return \Drupal::service('renderer')->render($build);
-  }
 
+    $content = $block_plugin->build();
+    dsm($block_plugin->getConfiguration());
+    if ($content && !Element::isEmpty($content)) {
+      if ($wrapper) {
+        $build = [
+          '#theme' => 'block',
+          '#attributes' => [],
+          '#contextual_links' => [],
+          '#configuration' => $block_plugin->getConfiguration(),
+          '#plugin_id' => $block_plugin->getPluginId(),
+          '#base_plugin_id' => $block_plugin->getBaseId(),
+          '#derivative_plugin_id' => $block_plugin->getDerivativeId(),
+          'content' => $content,
+        ];
+      } else {
+        $build = $content;
+      }
+    } else {
+      // Preserve cache metadata of empty blocks.
+      $build = [
+        '#markup' => '',
+        '#cache' => $content['#cache'],
+      ];
+    }
+
+    if (!empty($content)) {
+      CacheableMetadata::createFromRenderArray($build)
+        ->merge(CacheableMetadata::createFromRenderArray($content))
+        ->applyTo($build);
+    }
+
+    return $build;
+  }
 }
